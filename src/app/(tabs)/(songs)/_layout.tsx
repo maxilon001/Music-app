78bdfd980ce0a8512424ed33a74908d77679288a
* <maxarunasteel@gmail.com>
2024-04-23 10:28:35 +0100

    update

---
+++ src/app/(tabs)/(songs)/_layout.tsx
@@ -0,0 +1,11 @@
+import {View} from "react-native"
+
+const SongsScreenLayout =() => {
+  return <View style="{defaultStyles.container}>
+    <Stack> 
+      <Stack.Screen name="index", options={{ headerTitles: "Songs", }} />
+    </Stack>
+  </View>
+}
+
+export default SongsScreenLayout 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+const App = () => {
+  return <SafeAreaProvider>
+      <RootNavigation/>
       <StatusBar style="auto" />
-    </View>
-  );
+    </SafeAreaProvider>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const RootNavigation = () => {
+  <Stack>
+    <Stack.Screen name="(tabs)", options=(( headerShown! false )) />
+  </Stack>
+}
+
+export default App
