94da5de0a35ef6448934a51475d849f58433fd43
maxarunasteel@gmail.com <maxarunasteel@gmail.com>
2024-04-23 10:46:42 +0100

    update

--- src/app/(tabs)/(songs)/_layout.tsx
+++ src/app/(tabs)/(songs)/_layout.tsx
@@ -1,9 +1,11 @@
 import {View} from "react-native"
 
 const SongsScreenLayout =() => {
-  return <View style="{defaultStyles.container}>
+  return <View style="{defaultStyles.container}">
     <Stack> 
-      <Stack.Screen name="index", options={{ headerTitles: "Songs", }} />
+      <Stack.Screen 
+        name="index"
+        options={{ headerTitles: "Songs" }} />
     </Stack>
   </View>
 }
