00b8909eac31b3e393bd72f797085867e7f54bd3
* <maxarunasteel@gmail.com>
2024-04-26 16:19:19 +0100

    update

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
 import { colors, fontSize } from "@/constants/tokens"
+import { BlurView } from "expo-blur"
 import {Tabs} from "expo-router"
-
+import {StyleSheet} from 'react-native'
+import {FontAwesome, Ionicons, MaterialCommunityIcons,FontAwesome6} from '@expo/vector-icons'
 const TabsNavigation = () => {
   return ( 
     <Tabs screenOptions={{
@@ -9,10 +11,37 @@ const TabsNavigation = () => {
         fontSize: fontSize.xs,
 	fontWeight: '500',
       },
-      headerShown: false
+      headerShown: false,
+      tabBarStyle: {
+        position: 'absolute',
+	borderTopRightRadius: 20,
+	borderTopLeftRadius: 20,
+	borderTopWidth: 0,
+	paddingTop: 8
+      },
+      tabBarBackground: () => {
+        <BlurView 
+	  intensity={95}
+	  style={{
+	    ...StyleSheet.absoluteFillObject,
+	    overflow: 'hidden',
+	    borderTopLeftRadius: 20,
+	    borderTopRightRadius: 20,
+	  }}
+	/>
+      }
     }}>
-      <Tabs.Screen name="favourites" />
-      <Tabs.Screen name="playlists" />
+      <Tabs.Screen 
+        name="favourites"
+	options={{
+	  title: "Favourites",
+	  tabBarIcon: ({color}) => {
+	    <FontAwesome name= 'heart' size={20} color={ color } />
+	}}}
+      />
+      <Tabs.Screen name="playlists"
+        
+      />
       <Tabs.Screen name="(songs)" />
       <Tabs.Screen name="artists" />
     </Tabs>
